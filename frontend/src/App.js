5ae1a281ac51f511a53f2fa377adaf4008f955cf
MAbdurahman <mdbdrrhm@gmail.com>
2022-01-13 03:46:21 -0600

    Complete basic structure for Landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Landing from './pages/Landing';
 
 export default function App() {
   return (
     <div>
       <h2>jobTracker app</h2>
+      <Landing />
     </div>
   )
 }
---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Logo from './../assets/img/logo.svg';
+import MainImg from './../assets/img/main.svg';
+
+export default function Landing() {
+   return (
+      <div>
+         <h4>LandingPage</h4>
+      </div>
+   )
+}
