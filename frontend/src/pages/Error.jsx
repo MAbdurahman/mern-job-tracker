d169720216c4f074807c0c34ac20f2ae5c08b534
MAbdurahman <mdbdrrhm@gmail.com>
2022-01-14 15:00:54 -0600

    Complete Error.jsx

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NotFoundImage from './../assets/img/not-found.svg';
+import Wrapper from './../assets/wrappers/ErrorPage';
 
 export default function Error() {
    return (
-      <div>
-         <h2>ErrorPage</h2>
-      </div>
-   )
+		<Wrapper className='full-page'>
+			<div>
+				<img src={NotFoundImage} alt='not found' />
+				<h3>Ugh-oh! page not found</h3>
+				<h4>We cannot find that page.</h4>
+				<Link className='btn btn-error' to='/'>
+					back home
+				</Link>
+			</div>
+		</Wrapper>
+	);
 }
