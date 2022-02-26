ebc8d37433bbe38b6aebf53dfbe2463685f98cd2
MAbdurahman <mdbdrrhm@gmail.com>
2022-02-26 11:30:55 -0600

    Basic controllers/authController.js - registerUser test - passed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,16 +1,22 @@
+//**************** imports ****************//
+const User = require('./../models/user');
 
+/*===================================================
+   register a new user -> /api/v1/auth/register
+======================================================*/
+exports.registerUser = async (req, res) => {
+	try {
+      const user = await User.create(req.body);
+      res.status(201).json({user});
+   } catch (error) {
+      res.status(500).json({message: 'Error occurred!'});
+   }
+};
 
+exports.loginUser = async (req, res) => {
+	res.send('login user controller');
+};
 
-
-exports.registerUser = async(req, res) => {
-   res.send('register user controller');
-}
-
-exports.loginUser = async(req, res) => {
-   res.send('login user controller');
-}
-
-exports.updateUser = async(req, res) => {
-   res.send('update user controller');
-}
-
+exports.updateUser = async (req, res) => {
+	res.send('update user controller');
+};
