c1deac6c3c6c82fb4f0b3a7fdf2d3a638dbb5b5f
MAbdurahman <mdbdrrhm@gmail.com>
2022-02-27 07:39:09 -0600

    Add check for duplicate email in controller/authController.js - registerUser

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,11 +7,17 @@ const { BadRequestError, UnauthenticatedError } = require('./../errors');
       register a new user -> /api/v1/auth/register
 ============================================================*/
 exports.registerUser = async (req, res) => {
-      const { name, email, password } = req.body;
+	const { name, email, password } = req.body;
 
-      if (!name || !email || !password) {
-            throw new BadRequestError('Please provide all values!');
-      }
+	if (!name || !email || !password) {
+		throw new BadRequestError('Please provide all values!');
+	}
+
+	const userAlreadyExists = await User.findOne({ email });
+
+	if (userAlreadyExists) {
+		throw new BadRequestError('Email already exists!');
+	}
 
 	const user = await User.create({ name, email, password });
 	res.status(StatusCodes.CREATED).json({ user });
