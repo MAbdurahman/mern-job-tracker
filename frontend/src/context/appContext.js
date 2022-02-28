a544c0765fe0fd36d51d7b78aa0ca71c4c09fc7a
MAbdurahman <mdbdrrhm@gmail.com>
2022-02-28 07:15:41 -0600

    Complete frontend/RegisterUser - need correct error message -> backend

--- frontend/src/context/appContext.js
+++ frontend/src/context/appContext.js
@@ -1,4 +1,5 @@
 import React, { useState, useReducer, useContext } from 'react';
+import axios from 'axios';
 import reducer from './reducer';
 
 import {
@@ -37,7 +38,8 @@ const initialState = {
 	alertType: '',
 	user: null,
 	token: null,
-	userLocation: ''
+	userLocation: '',
+	jobLocation: '',
 };
 
 const AppContext = React.createContext();
@@ -56,9 +58,29 @@ const AppProvider = ({ children }) => {
 		}, 3500);
 	};
 
-	const registerUser = async (currentUser) => {
+	const registerUser = async currentUser => {
+		dispatch({ type: REGISTER_USER_BEGIN });
+		try {
+			const response = await axios.post('api/v1/auth/register', currentUser);
+
+			console.log(response);
+
+			const { user, token, location } = response.data;
+
+			dispatch({
+				type: REGISTER_USER_SUCCESS,
+				payload: { user, token, location },
+			});
+		} catch (error) {
+			console.log(error.response);
+			dispatch({
+				type: REGISTER_USER_ERROR,
+				payload: { msg: error.response.data.msg },
+			});
+		}
+		clearAlert();
 		console.log(currentUser);
-	}
+	};
 
 	return (
 		<AppContext.Provider
