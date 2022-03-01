c81cd0723904c88ace01d20b4c7614fdbc4f71c2
MAbdurahman <mdbdrrhm@gmail.com>
2022-03-01 08:53:22 -0600

    Add basic setup for nested dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Dashboard, Error, Landing, Register } from './pages';
+import { Error, Landing, Register } from './pages';
+import {
+	AddJob,
+	AllJobs,
+	Profile,
+	SharedLayout,
+	Stats 
+} from './pages/dashboard';
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<Dashboard />} />
+				<Route path='/' element={<div>temp-dashboard</div>} />
 				<Route path='/register' element={<Register />} />
 				<Route path='/landing' element={<Landing />} />
 
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,10 +1,9 @@
-import Dashboard from './dashboard/Dashboard';
+
 import Error from './Error';
 import Landing from './Landing';
 import Register from './Register';
 
 export {
-   Dashboard,
    Error,
    Landing,
    Register
