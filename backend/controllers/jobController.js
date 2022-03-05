6687cf98ec05196390351984d0ea262cb2e0c4be
MAbdurahman <mdbdrrhm@gmail.com>
2022-03-05 02:29:34 -0600

    Complete backend/controllers/jobController.js - updateJob

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -3,7 +3,11 @@ const { StatusCodes } = require('http-status-codes');
 const mongoose = require('mongoose');
 const moment = require('moment');
 const Job = require('./../models/job');
-const { BadRequestError, NotFoundError, UnauthenticatedError } = require('./../errors');
+const {
+	BadRequestError,
+	NotFoundError,
+	UnauthenticatedError,
+} = require('./../errors');
 const checkPermissions = require('./../utils/checkPermissions');
 
 exports.createJob = async (req, res) => {
@@ -18,11 +22,39 @@ exports.createJob = async (req, res) => {
 };
 
 exports.getAllJobs = async (req, res) => {
-	const jobs = await Job.find({createdBy: req.user.userId});
+	const jobs = await Job.find({ createdBy: req.user.userId });
 	if (!jobs) {
-		throw new NotFoundError(`No Jobs For User ${req.user.name}`)
+		throw new NotFoundError(`No Jobs For User ${req.user.name}`);
 	}
-	res.status(StatusCodes.OK).json({jobs, totalJobs: jobs.length, numOfPages: 1});
+	res.status(StatusCodes.OK).json({
+		jobs,
+		totalJobs: jobs.length,
+		numOfPages: 1,
+	});
+};
+
+exports.updateJob = async (req, res) => {
+	const { id: jobId } = req.params;
+	const { company, position } = req.body;
+
+	if (!position || !company) {
+		throw new BadRequestError('Please Provide All Values!');
+	}
+	const job = await Job.findOne({ _id: jobId });
+
+	if (!job) {
+		throw new NotFoundError(`No Job Found With ID: ${jobId}`);
+	}
+
+	//******** check whether user created the job ********//
+	checkPermissions(req.user, job.createdBy);
+
+	const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+		new: true,
+		runValidators: true,
+	});
+
+	res.status(StatusCodes.OK).json({ updatedJob });
 };
 
 exports.deleteJob = async (req, res) => {
@@ -31,8 +63,4 @@ exports.deleteJob = async (req, res) => {
 
 exports.showStats = async (req, res) => {
 	res.send('controller - showStats');
-};
-
-exports.updateJob = async (req, res) => {
-	res.send('controller - updateJob');
 };
