3fda38ffb6f86ad30e12a04c3421abe09cb3a151
MAbdurahman <mdbdrrhm@gmail.com>
2022-03-06 11:48:30 -0600

    Complete functionality of AreaChartContainer.jsx and BarChartContainer.jsx

--- frontend/src/components/ChartsContainer.jsx
+++ frontend/src/components/ChartsContainer.jsx
@@ -1,10 +1,20 @@
 import React, {useState} from 'react';
+import BarChart from './BarChartContainer';
+import AreaChart from './AreaChartContainer';
+import { useAppContext } from './../context/appContext';
 import Wrapper from './../assets/wrappers/ChartsContainer';
 
 export default function ChartsContainer() {
-   return (
-      <Wrapper>
-         <h4>Monthly Applications</h4>
-      </Wrapper>
-   )
+	//**************** variables ****************//
+	const [barChart, setBarChart] = useState(true);
+	const { monthlyApplications: data } = useAppContext();
+	return (
+		<Wrapper>
+			<h4>Monthly Applications</h4>
+			<button type='button' onClick={() => setBarChart(!barChart)}>
+				{barChart ? 'Area Chart' : 'Bar Chart'}
+			</button>
+			{barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+		</Wrapper>
+	);
 }
